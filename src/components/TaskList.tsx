05d44dc9b4a725d75c7610109d2dd42174f2c7c3
adriano.souza <adriano.souza@zpesystems.com>
2022-03-13 18:28:09 -0300

    Implementing task creation

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,6 +17,15 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(), // That is not used for real world, but for a study it's fine
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle(''); 
   }
 
   function handleToggleTaskCompletion(id: number) {
